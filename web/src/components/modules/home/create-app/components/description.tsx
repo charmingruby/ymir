547c29bb1391d9a0a5cca16f45da4fa9fa67aa71
charmingruby <mrrubycanal3105@gmail.com>
2023-06-20 10:12:06 -0300

    changed primary color on palette

--- web/src/components/modules/home/create-app/components/description.tsx
+++ web/src/components/modules/home/create-app/components/description.tsx
@@ -1,7 +1,7 @@
 export function NewAppDescription() {
   return (
-    <div>
-      <p className="text-lg leading-relaxed">
+    <div className="max-w-2xl">
+      <p className="text-lg leading-relaxed text-gray-50">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad expedita
         corrupti dolores aspernatur repudiandae? Nemo sint quasi animi labore
         dignissimos numquam omnis, quam delectus unde laudantium blanditiis
--- web/src/components/modules/home/create-app/index.tsx
+++ web/src/components/modules/home/create-app/index.tsx
@@ -5,15 +5,14 @@ import { NewAppActions } from './components/actions'
 
 export function CreateApp() {
   return (
-    <div className="border-b-2 border-gray-600 bg-primary-600 py-16">
+    <div className="bg-primary-900 relative overflow-hidden border-b-2 border-gray-900 py-16">
+      <div className="absolute right-1/2 -z-10 h-1/2 w-1/2 translate-x-1/2 bg-primary-300 blur-full" />
       <ContentContainer>
-        <div className="flex w-full">
-          {/* Content */}
-          <div className="flex flex-col justify-center gap-8">
-            <NewAppTitle />
-            <NewAppDescription />
-            <NewAppActions />
-          </div>
+        {/* Content */}
+        <div className="flex flex-col items-center gap-8">
+          <NewAppTitle />
+          <NewAppDescription />
+          <NewAppActions />
         </div>
       </ContentContainer>
     </div>

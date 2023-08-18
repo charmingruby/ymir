0a8993acff634aae1d027f018b1bb0d637d7ef64
charmingruby <mrrubycanal3105@gmail.com>
2023-08-18 14:41:18 -0300

    fixed members gap value

--- src/app/(landing)/about/components/Team/Members.tsx
+++ src/app/(landing)/about/components/Team/Members.tsx
@@ -10,7 +10,7 @@ export function Members() {
         <Users className="h-5 w-5 text-primary-300" />
         <div className="text-xl font-semibold">Meet the team</div>
       </div>
-      <div className="grid lg:grid-cols-3 gap-8">
+      <div className="grid lg:grid-cols-3 gap-4 md:gap-8">
         <MemberProfile
           avatarPath={gustavoProfile}
           name="Gustavo Dias"

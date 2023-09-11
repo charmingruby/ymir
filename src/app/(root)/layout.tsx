3718f3b2955583389b15220b774b515634cc1fec
charmingruby <mrrubycanal3105@gmail.com>
2023-09-11 08:59:40 -0300

    started foldering app

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import { Footer } from '@/app/(app)/(root)/components/footer'
-import { Header } from '@/app/(app)/(root)/components/header'
+import { Footer } from '@/app/(root)/components/footer'
+import { Header } from '@/app/(root)/components/header'
 import { ReactNode } from 'react'
 
 export default function DefaultLayout({ children }: { children: ReactNode }) {
